6556c98083d426fa916322c58fbe8fc033504fb0
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-03-03 15:56:06 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,8 @@
 def snake_it_up(string)
-  output = ' '
+  
   if string[0] == "s"
-    output = ("s" * 10) + string
+    ("s" * 10) + string
   else
-    output = string
+    string
   end
-  output
 end
